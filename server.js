1cd20d4aae2d0b7893e981045928b26182051f7b
yasos <saroyaute@gmail.com>
2025-12-17 12:10:53 +0100

    Refonte : Site E-commerce Neo Kicks

--- server.js
+++ server.js
@@ -5,10 +5,21 @@ const port = process.env.PORT || 3000;
 
 app.use(express.static('public'));
 
+// Route Accueil
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Route Boutique (Catalogue)
+app.get('/shop', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'pages', 'shop.html'));
+});
+
+// Route Panier (Simulé pour l'instant)
+app.get('/cart', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'pages', 'cart.html'));
 });
 
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+    console.log(`Server running on port ${port}`);
 });
