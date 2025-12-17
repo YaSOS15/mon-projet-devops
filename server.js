7ba35ef0c986248bbdf204f0312423ed70bcdf36
yasos <saroyaute@gmail.com>
2025-12-17 11:45:14 +0100

    Final: Site pro et notification Discord

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('Bonjour ! Mon application DevOps fonctionne ! Version 2 en ligne !');
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(port, () => {
-  console.log(`L'application écoute sur le port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
